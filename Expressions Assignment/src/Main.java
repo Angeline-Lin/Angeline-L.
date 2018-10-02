a7253c43a6107cacb22ebd20b25b1023a9a77d04
DoodleAssassin <43790482+DoodleAssassin@users.noreply.github.com>
2018-10-02 09:57:05 -0700

    test

---
+++ Expressions Assignment/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int rock = 1;
+        int paper = 2;
+        int scissors = 3;
+        String wordy
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter choice: ");
+        int wordy = reader.nextInt();
+        if (wordy > scissors) {
+            System.out.println("ok");
+        } else {
+            System.out.println("boo");
+        }
+    }
+}
