f7b6a87fa0e53a1d27d5e3d122cdf86d032033b5
DoodleAssassin <43790482+DoodleAssassin@users.noreply.github.com>
2018-10-11 09:48:53 -0700

    So far

--- Expressions Assignment/src/Main.java
+++ Expressions Assignment/src/Main.java
@@ -1,30 +1,58 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
 
-        int r = 1;
-        int p = 2;
-        int s = 3;
+        int word = 1;
 
+        //user input
         Scanner in = new Scanner(System.in);
         System.out.println("enter choice: ");
-        s = in.nextInt();
+        word = in.nextInt();
 
-        if (s == r) {
-            System.out.println("ok");
+        // random opponent
+        Random rand = new Random();
+        rand.nextInt(3);
+        int enemy = rand.nextInt(3)+1;
+
+        //rock,paper,scissors game
+        if (word == 1) {
+            System.out.println("rock");
+        }else if(word == 2){
+            System.out.println("paper");
         }else{
-            System.out.println("booooo");
+            System.out.println("scissors");
         }
 
-        if (r > 9) {
-            System.out.println("great");
-        }else {
-            System.out.println("try again");
+        //opponent choice
+        System.out.println("-------------");
+        if (enemy == 1) {
+            System.out.println("rock");
+        }else if(enemy == 2){
+            System.out.println("paper");
+        }else if(enemy == 3){
+            System.out.println("scissors");
         }
 
-
+        //game
+        System.out.println("~~~~~~");
+        if (word == enemy){
+            System.out.println("Try again");
+        }else if(word < enemy && word == 1 && enemy == 3){
+            System.out.println("You Win");
+        }else if(word < enemy && word == 1 && enemy == 2){
+            System.out.println("You lose");
+        }else if(word < enemy && word == 2){
+            System.out.println("You lose");
+        }else if(word > enemy && word == 2){
+            System.out.println("You win");
+        }else if(word > enemy && word == 3 && enemy == 1){
+            System.out.println("You lose");
+        }else if(word > enemy && word == 3 && enemy == 2){
+            System.out.println("You win");
+        }
 
     }
 }
