c212f30a2aba23c5c14a5e23af6e2cc013da2505
Angeline L <43790482+Angeline-Lin@users.noreply.github.com>
2019-01-16 10:56:31 -0800

    Add files via upload

---
+++ SchoolProject/src/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+
+        String firstname = "Bob";
+        String lastname = "Low";
+        String name = firstname + lastname;
+
+
+        int grade = 75;
+
+    Student(String name,int grade){
+        this.name = name;
+        this.grade = grade;
+    }
+
+
+
+}
---
+++ SchoolProject/src/Teacher.java
@@ -0,0 +1,19 @@
+public class Teacher {
+
+
+        String firstname = "John";
+        String lastname = "Mike";
+        String name = firstname + lastname;
+
+
+
+        String subject = "math";
+
+
+
+
+    Teacher(String name, String subject) {
+        this.name = name;
+        this.subject = subject;
+    }
+}
