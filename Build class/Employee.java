4742fae32fe3e5e6829b383e5bf0f777f6e2d47a
Angeline L <43790482+Angeline-Lin@users.noreply.github.com>
2019-01-18 11:02:30 -0800

    Add files via upload

---
+++ Build class/Employee.java
@@ -0,0 +1,26 @@
+public class Employee {
+    private int id;
+
+    private String Firstname;
+
+    private String Lastname;
+
+    private int Salary;
+
+
+    Employee(int id, String Firstname, String Lastname, int Salary){
+        this.id = id;
+        this.Firstname = Firstname;
+        this.Lastname = Lastname;
+        this.Salary = Salary;
+
+    }
+public int getId(){
+    return id;
+}
+
+
+
+
+
+}
