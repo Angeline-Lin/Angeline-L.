b295a3a9eae8fad72e58f116ba3b7331d2717586
Angeline L <43790482+Angeline-Lin@users.noreply.github.com>
2019-01-18 11:08:11 -0800

    Add files via upload

---
+++ Employee/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int num = 10;
+
+        String name = "name";
+
+        Employee ee = new Employee(10);
+
+        System.out.println(ee.getId());
+
+    }
+
+}}
