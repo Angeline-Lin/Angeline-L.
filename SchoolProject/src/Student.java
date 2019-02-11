6e5c096896eb8173cb8ae833f41833a5139bcac6
Angeline L <43790482+Angeline-Lin@users.noreply.github.com>
2019-02-11 12:45:25 -0800

    Add files via upload

--- SchoolProject/src/Student.java
+++ SchoolProject/src/Student.java
@@ -1,17 +1,36 @@
 public class Student {
 
-        String firstname = "Bob";
-        String lastname = "Low";
-        String name = firstname + lastname;
+    // student name
+    private String firstname;
+    private String lastname;
+    private String name = firstname + lastname;
 
 
-        int grade = 75;
+    //student grade
+    private int grade;
 
-    Student(String name,int grade){
+    //student number
+    private int num;
+
+//Constructor
+    Student(String name, int grade) {
         this.name = name;
         this.grade = grade;
+        num++;
     }
 
 
+    //getters and setters
+    public int getGrade() {
+        return grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNum() {
+        return num;
+    }
 
 }
--- SchoolProject/src/Teacher.java
+++ SchoolProject/src/Teacher.java
@@ -1,19 +1,32 @@
 public class Teacher {
 
-
-        String firstname = "John";
-        String lastname = "Mike";
-        String name = firstname + lastname;
-
-
-
-        String subject = "math";
+    //Teacher name
+       private String firstName;
+       private String lastName;
+       private String name = firstName + lastName;
 
 
+    //Teacher subject
+       private String subject;
 
-
+//Constructor
     Teacher(String name, String subject) {
         this.name = name;
         this.subject = subject;
     }
+
+    //getters and setters
+    public String getName(){
+
+        return name;
+    }
+    public String getSubject(){
+
+        return subject;
+}
+
+
+
+
+
 }
